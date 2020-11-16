324f18ab2a840797521243579d8f9091634df0a5
agiknightt <70384293+agiknightt@users.noreply.github.com>
2020-11-16 15:51:11 +0700

    Add files via upload

---
+++ Scripts/Mover.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            MoverPosition(_speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            MoverPosition(_speed * Time.deltaTime * -1);
+        }
+    }
+
+    private void MoverPosition(float x = 0, float y = 0)
+    {
+        transform.Translate(x, y, 0);
+    }
+}
